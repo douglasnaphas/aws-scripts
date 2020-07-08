9bccb959f302230c3a40cde38977d98c38e6e8ca
Douglas Naphas <douglasnaphas@gmail.com>
2020-07-08 16:30:42 -0700

    List buckets

--- scripts/bucket-size.sh
+++ scripts/bucket-size.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 echo "hello, \$1 is $1"
+KEYWORD=$1
+
+# get all the buckets of interest
+aws s3api list-buckets | \
+  jq '.Buckets | map(select(.Name | test("'${KEYWORD}'")))'
+
+# for each
+
+  # get the 
