6634812fa1675618afe8d0502b1cf72a45a4719f
Douglas Naphas <douglasnaphas@gmail.com>
2020-07-08 17:34:17 -0700

    Format bucket-size output

--- scripts/bucket-size.sh
+++ scripts/bucket-size.sh
@@ -10,6 +10,10 @@ do
   aws s3api list-object-versions --bucket ${bucket} | \
     jq '.Versions[] | .Size' | \
     awk -v bucket=${bucket} \
-      '{s = s + $1} END {printf("%'"'"'d\t%.2f\t%s\n", s, s * .023 / 1000 / 1000 / 1000, bucket)}'
+      '{s = s + $1}
+       END {
+         printf("%17s   %s   %s\n", "BYTES", "MAX COST", "BUCKET");
+         printf("%'"17'"'d   %8.2f   %s\n", s, s * .023 / 1000 / 1000 / 1000, bucket);
+       }'
 done
 
